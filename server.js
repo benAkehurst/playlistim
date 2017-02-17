67bada5d01f0d5567907a8a90774f2119990d41d
mistabenjo <benakehurst@hotmail.com>
2017-02-17 23:27:12 +0200

    added post option

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // Sets index.html as the homepage
 app.use(express.static(__dirname));
 
-// Mongoose connets us to the MongoDB database
+// Mongoose connects us to the MongoDB database
 mongoose.connect("mongodb://localhost:27017/playlistimDB", function(err){
 	if(err){
 		console.log("Error: " + err);
@@ -23,6 +23,24 @@ mongoose.connect("mongodb://localhost:27017/playlistimDB", function(err){
 		console.log("Connected to MongoDB")
 	}
 });
+
+// Create a schema for a video
+var Video = mongoose.model("Video", {
+	title: String,
+	category: String,
+	description: String,
+	link: String
+});
+
+// Add a new video
+app.post("/add", function(request,response){
+	var video = new Video(request.body);
+	video.save();
+	response.status(201);
+	response.send(video);
+});
+
+
 
 // Allows the server to run on localhost
 app.listen(3000, function(){
